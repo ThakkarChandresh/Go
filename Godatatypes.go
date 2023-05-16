5976be92b61f2d970ca1fb9692ee4b763a68f6ad
mihir <ma@g.com>
2023-05-16 12:11:00 +0530

    hello go

---
+++ Godatatypes.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+	//a := 20
+	//fmt.Println(a)
+	//
+	//a, b := 20, 10
+	//fmt.Println(a, b)
+	//
+	//var x = 10.4
+	//const y = 30
+	//fmt.Println(x * y)
+
+}
---
+++ HelloWorld.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt is package for printing function
+	fmt.Print("Hello World")
+}
