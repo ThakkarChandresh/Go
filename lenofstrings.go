d92f14ba66acdecd00959ee13617e012291daa2f
mihir <ma@g.com>
2023-05-20 18:34:58 +0530

    comments added

--- lenofstrings.go
+++ lenofstrings.go
@@ -4,8 +4,23 @@ import "fmt"
 
 func main() {
 
-	//
+	//len prints the number of bytes variable value takes
+
+	//each character is ascii
 	var name string = "mihir"
-	fmt.Println("Length: ", len(name))
+	fmt.Println("Length: ", len(name)) //print '5'
+
+	//string where in character takes more than byte
+	//here visually string is of 5 length but underneath
+	//each character takes 2 bytes and hence go prints
+	//10 since it always prints the actual memory taken
+	//by string in bytes
+	name = "èèèçÿ"
+	fmt.Println("Length: ", len(name)) //prints '10'
+
+	//how to get actual number of characters
+	//here casting to array of 'rune' datatype will help
+	//prints 5
+	fmt.Println("Actual characters no: ", len([]rune(name)))
 
 }
